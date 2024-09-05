0f831dfdad9adae372a55a6599a609b851544ff5
Rafael sosa <rafa.sosa20@gmail.com>
2024-09-05 18:52:40 -0400

    primer commit del ejercicio 2

---
+++ ejercicio_2.php
@@ -0,0 +1,10 @@
+<?php
+date_default_timezone_set("America/Lima");
+$fecha = date("d-m-Y");
+$hora = date("H:i:s");
+
+echo "Hora de America/Lima<br>";
+echo "La fecha actual es: $fecha <br>";
+echo "La hora actual es: $hora";
+
+?>
